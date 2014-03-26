c3c20f8e666811ea2ecc8ef87537df91d56aa121
Alex Hartsell <kalhartt@gmail.com>
2014-03-26 16:59:57 -0400

    Add recalc_map_points

--- dbutils/rs_utils_old.py
+++ dbutils/rs_utils_old.py
@@ -8,6 +8,10 @@ import rs_models_old as models
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# points for each rank
+_pointmap = [40, 34, 31] + list(range(27, 0, -1))
+
+# Configure the db connection
 _RSNAME = os.getenv('RSNAME', None)
 _RSPASS = os.getenv('RSPASS', None)
 _RSHOST = os.getenv('RSHOST', None)
@@ -17,3 +21,41 @@ _engine = create_engine('mysql+pymysql://{}:{}@{}/{}'.format(
 _session = sessionmaker()
 _session.configure(bind=_engine)
 session = _session()
+
+
+def recalc_map_points(mid):
+    """
+    Recalculate the points for each record on a map
+
+    args:
+        mid (int): ID of the map to recalculate
+    """
+    pm = models.PlayerMap
+    records = session.query(pm).\
+                      filter(pm.map_id == mid,
+                             pm.time != None,
+                             pm.prejumped == 'false').\
+                      order_by(pm.time).all()
+
+    rank = -1
+    last_time = None
+    for n, record in enumerate(records):
+
+        # increase rank if not tied w/ last
+        if record.time != last_time:
+            rank += 1
+
+            # Break out if the race doesnt get points
+            if rank >= len(_pointmap):
+                break
+        last_time = record.time
+        record.points = _pointmap[rank]
+
+    # Set all other records to 0 points
+    session.query(pm).\
+            filter(pm.map_id == mid,
+                   (pm.prejumped == 'true') |
+                   (pm.time == None) |
+                   (pm.time > last_time)).\
+            update({pm.points:0})
+    session.commit()
