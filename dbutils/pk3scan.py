51ecfedbf9c6a71d3d7454cbfe44ab724a4c492f
Alex Hartsell <kalhartt@gmail.com>
2014-04-19 12:05:11 -0400

    Add new models for racesow db

--- dbutils/pk3scan.py
+++ dbutils/pk3scan.py
@@ -11,11 +11,62 @@ Scans a folder for pk3 files and checks them for the following:
 
 """
 import logging
+import os
+import re
 import sys
-from zipfile import ZipFile
-from pathlib import Path
+from rs_models import Map, Tag
 from docopt import docopt
+from pathlib import Path
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from zipfile import ZipFile
 
+
+# Configure the db connection
+_engine = create_engine(os.getenv('PK3SCANDB'))
+_session = sessionmaker()
+_session.configure(bind=_engine)
+session = _session()
+
+
+# A dict mapping entity classnames to a maptag
+_TAGMAP = {
+    # 'weapon_grapplinghook', 'gh',
+    'weapon_gauntlet': 'gb',
+    'weapon_gunblade': 'gb',
+    'weapon_machinegun': 'eb',
+    'weapon_rocketlauncher': 'rl',
+    'weapon_plasmagun': 'pg',
+    'weapon_shotgun': 'rg',
+    'weapon_riotgun': 'rg',
+    'weapon_grenadelauncher': 'gl',
+    'weapon_lightning': 'lg',
+    'weapon_electrobolt': 'eb',
+    'weapon_railgun': 'eb',
+    'weapon_bfg': 'pg',
+    'weapon_instagun': 'ig'
+}
+
+def get_or_create(obj, flt):
+    """
+    Get or create the tag with a given name
+
+    Args:
+        obj - The Model object or table to get from
+        flt - a kwarg dict to pass to filter_by or create the object with
+
+    Returns:
+        A tuple of (instance, created)
+    """
+    result = session.query(obj).filter_by(**flt).first()
+    found = False
+
+    if not result:
+        result = obj(**flt)
+        found = True
+        session.add(result)
+
+    return result, found
 
 def parse_shader(shader):
     """
@@ -38,7 +89,32 @@ def parse_shader(shader):
 
     return result
 
-    
+
+def parse_bsp(bspfile, mapobj):
+    """
+    Add a map to the database and tag it according to entities on the map
+
+    Args:
+        bspfile - The bspfile of the map
+        mapobj - Instance of the map model to update
+    """
+    logger = logging.getLogger('parse_bsp')
+    tags = set()
+    weaponre = re.compile(r'\"classname\" \"(weapon_[a-z]*)\"',
+        flags=re.IGNORECASE)
+
+    for line in bspfile:
+        line = line.decode('ascii', 'ignore').strip()
+        matches = weaponre.findall(line)
+        if matches:
+            for match in matches:
+                if not match in _TAGMAP:
+                    logger.info('Unknown weapon: {}'.format(match))
+                    continue
+                tag, created = get_or_create(Tag, {'name': _TAGMAP[match]})
+                tags.add(tag)
+    return tags
+
 
 def scan(pk3dir, basedir):
     """
@@ -87,7 +163,6 @@ def scan(pk3dir, basedir):
                                     .format(pk3file, texture))
 
 
-
 def update(pk3dir):
     """
     Update the database with map information parsed from pk3 files in folder
@@ -95,7 +170,36 @@ def update(pk3dir):
     Args:
         pk3dir - Path to a directory containing pk3 files to scan and update
     """
-    pass
+    logger = logging.getLogger('update')
+    pk3path = Path(pk3dir)
+
+    if not pk3path.is_dir():
+        logger.error('{} is not a valid directory'.format(pk3path))
+        sys.exit(1)
+
+    for pk3file in pk3path.glob('*.pk3'):
+        pk3zip = ZipFile(str(pk3file))
+        mapfiles = [x for x in pk3zip.namelist() if x.endswith('.bsp')]
+
+        if not mapfiles:
+            logger.info('pk3 {} contains no maps, skipping'.format(pk3file))
+            continue
+
+        for mapfile in mapfiles:
+            logger.debug('Parsing bsp: {}'.format(mapfile))
+            mapobj, created = get_or_create(Map, {'name': mapfile[5:-4]})
+            if not created:
+                continue
+
+            tags = parse_bsp(pk3zip.open(mapfile), None)
+            if not tags:
+                tag, created = get_or_create(Tag, {'name': 'strafe'})
+                tags.add(tag)
+
+            mapobj.pk3file = str(pk3file)
+            mapobj.tags.extend(tags)
+            session.commit()
+            logger.info('Added map {} with tags: {}'.format(mapfile[5:-4], tags))
 
 
 def main():
---
+++ dbutils/rs_models.py
@@ -0,0 +1,100 @@
+"""
+rs_models
+
+Provides SQLAlchemy orm models for the racesow database
+"""
+from datetime import datetime
+from sqlalchemy import (
+    BigInteger,
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    Enum,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+    Table,
+    Text,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+_Base = declarative_base()
+
+
+_MAPTAG = Table('racesow_map_tags', _Base.metadata,
+    Column('map_id', Integer, ForeignKey('racesow_map.id')),
+    Column('tag_id', Integer, ForeignKey('racesow_tag.id'))
+)
+
+
+class Tag(_Base):
+    """Tag for describing a map"""
+    __tablename__ = 'racesow_tag'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return '<Tag(id={}, name="{}")>'.format(self.id, self.name)
+
+
+class Server(_Base):
+    """Racesow Server Model"""
+    __tablename__ = 'racesow_server'
+
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer) # Todo - this is a foreign key to user
+    address = Column(String)
+    auth_key = Column(String)
+    name = Column(String)
+    simplified = Column(String)
+    players = Column(Text)
+    playtime = Column(BigInteger)
+    races = Column(Integer)
+    created = Column(DateTime, default=datetime.utcnow)
+    last_seen = Column(DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Server(id={}, simp="{})>'.format(self.id, self.simplified)
+
+
+class Map(_Base):
+    """Racesow Map Model"""
+    __tablename__ = 'racesow_map'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    pk3file = Column(String, default='')
+    levelshotfile = Column(String, default='')
+    enabled = Column(Boolean, default=True)
+    races = Column(Integer, default=0)
+    playtime = Column(BigInteger, default=0)
+    created = Column(DateTime, default=lambda x: datetime.utcnow())
+    oneliner = Column(String, default='')
+    tags = relationship('Tag', secondary=_MAPTAG, backref='maps')
+
+    def __repr__(self):
+        return '<Map(id={}, name="{}")>'.format(self.id, self.name)
+
+
+class Player(_Base):
+    __tablename__ = 'racesow_player'
+
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer) # Todo: this is a foreign key to user
+    admin = Column(Boolean)
+    name = Column(String)
+    simplified = Column(String)
+    playtime = Column(BigInteger)
+    races = Column(Integer)
+    maps = Column(Integer)
+    points = Column(Integer)
+    skill_m = Column(Float)
+    skill_s = Column(Float)
+
+    def __repr__(self):
+        return '<Player(id={}, simp="{}")>'.format(self.id, self.simplified)
